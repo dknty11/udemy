91a7270c4447193964ba603ac767b610e6f81be5
dknty11 <nguyenhoaivinh.1111@gmail.com>
2019-03-19 16:54:12 +0700

    connect mongodb and create new app for users and tasks

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,39 +1,13 @@
-// setTimeout(() => {
-//     console.log('2 seconds')
-// }, 2000)
-
-// const names = ['A', 'B', 'C']
-// const shortNames = names.filter((name) => {
-//     return name.length <= 4
-// })
-
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitide: 0,
-//             longtitude: 0
-//         }
-
-//         callback(data)
-//     }, 2000)
-
-// }
-
-// geocode('Los Angeles', (data) => {
-//     console.log(data)
-// })
-
-// add(1, 4, (sum) => {
-//     console.log(sum)
-// })
-
-const add = (x, y, callback) => {
+const doWorkCallback = (callback) => {
     setTimeout(() => {
-        sum = x + y
-        callback(sum)
+        // callback('This is error', undefined)
+        callback(undefined, [1,4,5])
     }, 2000)
 }
 
-add(1, 4, (sum) => {
-    console.log(sum)
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+    console.log(result)
 })
